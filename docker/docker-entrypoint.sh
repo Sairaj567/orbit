e7069fb250d73ee70c2eb6408ef93169c9433354
Sairaj567 <Sairajnagargoje567@gmial.com>
2026-07-26 23:52:13 +0530

    docker entery point error

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "==> Running Prisma database migrations (prisma migrate deploy)..."
-npx prisma migrate deploy --schema=apps/api/prisma/schema.prisma
+./apps/api/node_modules/.bin/prisma migrate deploy --schema=apps/api/prisma/schema.prisma
 
 echo "==> Migration check completed. Starting Orbit API..."
 exec "$@"
